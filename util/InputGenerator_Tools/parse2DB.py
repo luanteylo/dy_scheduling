54749c8c47ec744c1dfe515fbd21e45ab0be0588
Luan Teylo <luanteylo@gmail.com>
2018-10-05 08:56:49 -0300

    doc samples

--- util/InputGenerator_Tools/parse2DB.py
+++ util/InputGenerator_Tools/parse2DB.py
@@ -1,29 +1,35 @@
 import glob, os
 import database as db
 
-parse_dir = "/home/luan/google-clusterdata/clusterdata-2011-2/parse/"
+parse_dir = "/home/luan/google_trace/clusterdata-2011-2/parse/"
+
+instances = []
 
 
+# def loadInstances():
+#     conn = db.create_connection("db.bin")
 
 
 def parse2DB():
     os.chdir(parse_dir)
     onlyfiles = glob.glob("*.csv")
 
-    for file in onlyfiles:
+    conn = db.create_connection("/home/luan/project/phd_project/dy_scheduling/util/InputGenerator_Tools/db.bin")
 
-        conn = db.create_connection("db.bin")
+    with conn:
 
-        with open(file) as f:
+        for file in onlyfiles:
+
             print file
-            for line in f.readlines():
-                jobid = line.split(",")[0]
-                taskid = line.split(",")[1]
-                memory = line.split(",")[4]
-                runtime = line.split(",")[5]
 
-                db.insert_task(conn, job_id, taskid)
+            with open(file) as f:
+                for line in f.readlines():
+                    jobid = line.split(",")[0]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+                    taskid = line.split(",")[1]
+                    memory = line.split(",")[4]
+                    runtime = line.split(",")[5]
 
+                    db.insert_task(conn, int(jobid), int(taskid), "t_" + taskid, float(memory), 0.0)
 
 
 

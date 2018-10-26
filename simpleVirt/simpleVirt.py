ef6fedb5dd82c1eb2d05e827c34b35eb40dd2a5e
Theusfilipe <theusfilipe@gmail.com>
2018-10-26 15:52:31 -0300

    google basic methods work

--- simpleVirt/simpleVirt.py
+++ simpleVirt/simpleVirt.py
@@ -6,7 +6,8 @@ import paramiko
 
 import boto3
 from botocore.exceptions import ClientError, ConfigParseError
-
+import googleapiclient.discovery
+from six.moves import input
 
 class simpleVirt:
 
@@ -474,3 +475,112 @@ class simpleVirt:
     def log_print(self,msg, verbose):
         if verbose:
             print msg
+
+
+    #google functions
+    
+    def get_google_compute():
+        return googleapiclient.discovery.build('compute', 'v1')
+    
+    def wait_for_operation(compute, project, zone, operation): 
+        print('Waiting for operation to finish...')
+        while True:
+            result = compute.zoneOperations().get(
+            project=project,
+            zone=zone,
+            operation=operation).execute()
+
+            if result['status'] == 'DONE':
+                print("done.")
+                if 'error' in result:
+                    raise Exception(result['error'])
+                return result
+
+            time.sleep(1)
+    
+    def create_instance(compute, project, zone, name, bucket, vcpu_number, mem_number):
+        # Get the latest Debian Jessie image.
+        image_response = compute.images().getFromFamily(
+        project='ubuntu-os-cloud', family='ubuntu-1804-lts').execute()
+        source_disk_image = image_response['selfLink']
+
+        # Configure the machine
+        machine_type = "zones/%s/machineTypes/custom-%s-%s" % (zone, str(vcpu_number), str(mem_number*1024))
+        startup_script = open(
+        os.path.join(
+            os.path.dirname(__file__), 'startup-script.sh'), 'r').read()
+        image_url = "http://storage.googleapis.com/gce-demo-input/photo.jpg"
+        image_caption = "Ready for dessert?"
+
+        config = {
+            'name': name,
+            'machineType': machine_type,
+
+            # Specify the boot disk and the image to use as a source.
+            'disks': [
+                {
+                    'boot': True,
+                    'autoDelete': True,
+                    'initializeParams': {
+                    'sourceImage': source_disk_image,
+                    }
+                }
+            ],
+
+            # Specify a network interface with NAT to access the public
+            # internet.
+            'networkInterfaces': [{
+                'network': 'global/networks/default',
+                'accessConfigs': [
+                    {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+                ]
+            }],
+
+            # Allow the instance to access cloud storage and logging.
+            'serviceAccounts': [{
+                'email': 'default',
+                'scopes': [
+                    'https://www.googleapis.com/auth/devstorage.read_write',
+                    'https://www.googleapis.com/auth/logging.write'
+                ]
+            }],
+
+            # Metadata is readable from the instance and allows you to
+            # pass configuration from deployment scripts to instances.
+            'metadata': {
+                'items': [{
+                    # Startup script is automatically executed by the
+                    # instance upon startup.
+                    'key': 'startup-script',
+                    'value': startup_script
+                }, {
+                    'key': 'url',
+                    'value': image_url
+                }, {
+                    'key': 'text',
+                    'value': image_caption
+                }, {
+                    'key': 'bucket',
+                    'value': bucket
+                }]
+            }
+        }
+        return compute.instances().insert(
+            project=project,
+            zone=zone,
+            body=config).execute()
+    
+    def delete_instance(compute, project, zone, name):
+        return compute.instances().delete(
+            project=project,
+            zone=zone,
+            instance=name).execute()
+        
+    def list_instances(compute, project, zone):
+        result = compute.instances().list(project=project, zone=zone).execute()
+        return result['items']        
+            
+            
+            
+            
+            
